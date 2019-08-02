6349b0e27c697af9134c478fc9c504222036a27f
yushizhao <yushizhao@outlook.com>
2019-08-02 10:18:11 +0000

    on_assign

--- test-consumer.py
+++ test-consumer.py
@@ -1,5 +1,20 @@
-from confluent_kafka import Consumer, KafkaError
+from confluent_kafka import Consumer, KafkaError, TopicPartition
 
+def print_partition(p):
+    print(f"assigned: {p.topic}.{p.partition} at {p.offset}")
+
+def print_msg(msg):
+    print(f"received: {msg.key()} : {msg.value()} of {msg.topic()}.{msg.partition()} at {msg.offset()}")
+
+def my_on_assign(consumer, partitions):
+    for p in partitions:
+            # some starting offset, or use OFFSET_BEGINNING, et, al.
+            # the default offset is STORED which means use committed offsets, and if
+            # no committed offsets are available use auto.offset.reset config (default latest)
+        print_partition(p)
+        # p.offset = 10
+    # call assign() to start fetching the given partitions.
+    # consumer.assign(partitions)
 
 if __name__ == "__main__":
 
@@ -13,7 +28,7 @@ if __name__ == "__main__":
         'sasl.password': 'bobpassword'
     })
 
-    c.subscribe(['test'])
+    c.subscribe(['test'],on_assign=my_on_assign)
 
     while True:
         msg = c.poll(1.0)
@@ -24,6 +39,6 @@ if __name__ == "__main__":
             print("Consumer error: {}".format(msg.error()))
             continue
 
-        print('Received message: {}'.format(msg.value().decode('utf-8')))
-
+        print_msg(msg)
+        
     c.close()
--- test-list.py
+++ test-list.py
@@ -1,15 +1,42 @@
-from confluent_kafka import Producer
+from confluent_kafka import Producer, Consumer, TopicPartition
 import jsonpickle
 
 p = Producer({
-    'bootstrap.servers': '127.0.0.1:9093,127.0.0.1:9094,127.0.0.1:9095',
-    'security.protocol': 'SASL_PLAINTEXT',
-    'sasl.mechanisms': 'PLAIN',
-    'sasl.username': 'alice',
-    'sasl.password': 'alicepassword'
+        'bootstrap.servers': '127.0.0.1:9093,127.0.0.1:9094,127.0.0.1:9095',
+        'security.protocol': 'SASL_PLAINTEXT',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': 'admin',
+        'sasl.password': 'adminpassword'
+    })
+
+c = Consumer({
+        'bootstrap.servers': '127.0.0.1:9094',
+        'group.id': 'bob-group',
+        'auto.offset.reset': 'earliest',
+        'security.protocol': 'SASL_PLAINTEXT',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': 'bob',
+        'sasl.password': 'bobpassword'
     })
 
 if __name__ == "__main__":
     
-    res = p.list_topics("test")
-    print(jsonpickle.encode(res))
+    tptest0 = TopicPartition(topic="test",partition=0)
+    tptest1 = TopicPartition(topic="test",partition=1)
+    tptest2 = TopicPartition(topic="test",partition=2)
+
+    # res = p.list_topics()
+    # print(jsonpickle.encode(res))
+
+    tps = c.committed([tptest0,tptest1,tptest2])
+    for tp in tps:
+        print(tp.partition)
+        print(tp.offset)
+
+    # c.assign([TopicPartition(topic="test",partition=2,offset=0)])
+    asss = c.assignment()
+    print(asss)
+    for ass in asss:
+        print(ass.topic)
+        print(ass.partition)
+        print(ass.offset)
